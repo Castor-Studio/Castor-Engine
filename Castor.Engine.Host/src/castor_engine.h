193afa4d9d4f9dd3e8252f4a100d7663b02a5d9c
enzo-cesaretti <114489047+enzo-cesaretti@users.noreply.github.com>
2026-07-21 00:09:27 +0200

    build(host): bootstrap native CMake library

---
+++ Castor.Engine.Host/src/castor_engine.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+
+#if defined(_WIN32)
+    #if defined(CASTOR_ENGINE_HOST_EXPORTS)
+        #define CASTOR_ENGINE_API __declspec(dllexport)
+    #else
+        #define CASTOR_ENGINE_API __declspec(dllimport)
+    #endif
+#else
+    #define CASTOR_ENGINE_API
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define CASTOR_ENGINE_ABI_VERSION 1
+#define CASTOR_ENGINE_VERSION "0.1.0-alpha.1"
+
+CASTOR_ENGINE_API uint32_t castor_engine_get_abi_version(void);
+
+CASTOR_ENGINE_API const char* castor_engine_get_version(void);
+
+#ifdef __cplusplus
+}
+#endif
