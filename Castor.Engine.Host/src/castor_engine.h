2c6f4045818d9f6b4fcbd36719ec8c862e11dcc8
enzo-cesaretti <114489047+enzo-cesaretti@users.noreply.github.com>
2026-07-25 00:30:13 +0200

    fix(obs): wire SDK bootstrap into engine build

--- Castor.Engine.Host/src/castor_engine.h
+++ Castor.Engine.Host/src/castor_engine.h
@@ -3,13 +3,13 @@
 #include <stdint.h>
 
 #if defined(_WIN32)
-    #if defined(CASTOR_ENGINE_HOST_EXPORTS)
-        #define CASTOR_ENGINE_API __declspec(dllexport)
-    #else
-        #define CASTOR_ENGINE_API __declspec(dllimport)
-    #endif
+#if defined(CASTOR_ENGINE_HOST_EXPORTS)
+#define CASTOR_ENGINE_API __declspec(dllexport)
 #else
-    #define CASTOR_ENGINE_API
+#define CASTOR_ENGINE_API __declspec(dllimport)
+#endif
+#else
+#define CASTOR_ENGINE_API
 #endif
 
 #ifdef __cplusplus
@@ -21,7 +21,9 @@ extern "C" {
 
 CASTOR_ENGINE_API uint32_t castor_engine_get_abi_version(void);
 
-CASTOR_ENGINE_API const char* castor_engine_get_version(void);
+CASTOR_ENGINE_API const char *castor_engine_get_obs_version(void);
+
+CASTOR_ENGINE_API const char *castor_engine_get_version(void);
 
 #ifdef __cplusplus
 }
